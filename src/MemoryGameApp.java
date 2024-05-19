9035ad62487f11c9716ecd46ca692f5916da417f
RehamTammam1 <rtammam0@gmail.com>
2024-05-19 18:27:32 +0300

    adding labels to input fields

--- src/MemoryGameApp.java
+++ src/MemoryGameApp.java
@@ -7,51 +7,57 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class MemoryGameApp extends JFrame {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
 
         // Memory Game app
         JFrame frame;
         Boarder boarder = new Boarder();
         JTextField numberOfrows = new JTextField();
         JTextField numberOfColumns = new JTextField();
-        numberOfrows.setPreferredSize(new Dimension(100,30));
-        numberOfColumns.setPreferredSize(new Dimension(100,30));
-
+        JTextField timerinput = new JTextField();
+        numberOfrows.setPreferredSize(new Dimension(100, 30));
+        numberOfColumns.setPreferredSize(new Dimension(100, 30));
+        timerinput.setPreferredSize(new Dimension(100, 30));
         JButton enterBtn = new JButton("Enter");
-        frame = new JFrame("WELVOME");
+        frame = new JFrame("Welcome to Memory Game");
         frame.setVisible(true);
-        frame.setSize(500,500);
+        frame.setSize(500, 500);
 
         JPanel panel = new JPanel();
         panel.setLayout(new FlowLayout());
-        JLabel label = new JLabel("My label");
-        panel.add(label);
+        JLabel columnLabel = new JLabel("Columns");
+        JLabel rowLabel = new JLabel("Rows");
+        JLabel timerLabel = new JLabel("Timer");
+
         frame.add(panel);
-        panel.add(enterBtn);
+        panel.add(columnLabel);
         panel.add(numberOfColumns);
+        panel.add(rowLabel);
         panel.add(numberOfrows);
+        panel.add(timerLabel);
+        panel.add(timerinput);
+        panel.add(enterBtn);
+        // Retrieve input value from text field
+
         enterBtn.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                // Retrieve input value from text field
                 int rowsValue = Integer.parseInt(numberOfrows.getText());
                 int columnsValue = Integer.parseInt(numberOfColumns.getText());
-                int totalCards = rowsValue* columnsValue ;
-                if (!boarder.checkBoarderSize(rowsValue,columnsValue)) {
+                int totalCards = rowsValue * columnsValue;
+                if (!boarder.checkBoarderSize(rowsValue, columnsValue)) {
                     JOptionPane.showMessageDialog(null, "Boarder Size should be even");
-                }else{
+                } else {
                     //Create a boarder
-                   boarder.createBorader(frame,true);
+                    boarder.createBorader(frame, true);
                     //Create a array of cards
                     CardsManger manger = new CardsManger();
-                    Card[] cards=manger.createCardsCollection(rowsValue,columnsValue,totalCards,frame);
-                    //create array of buttons with similar backgounds
-                    System.out.println(cards.length);
-                    System.out.println(cards[1].getCardValue());
-                    System.out.println(cards[1].getCardView());
+                    Card[] cards = manger.createCardsCollection(rowsValue, columnsValue, totalCards, frame);
+
+
                 }
             }
-            //on cliking any of the buttons
+
         });
     }
 }
