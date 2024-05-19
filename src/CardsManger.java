e92d3092cfe8b78a30d8046a245049de1e8427d7
RehamTammam1 <rtammam0@gmail.com>
2024-05-19 22:32:59 +0300

    Refactor CardsManger Class

--- src/CardsManger.java
+++ src/CardsManger.java
@@ -6,44 +6,82 @@ import java.util.Random;
 import javax.swing.border.EmptyBorder;
 
 public class CardsManger {
+    int padding = 10;
+   private int numberOfRows;
+   private int numberOfColumns;
+    int numberOfCards;
+    RandomColor cardColor = new RandomColor();
+    public CardsManger(int numberOfRows,int numberOfColumns){
+        this.numberOfRows = numberOfRows;
+        this.numberOfColumns = numberOfColumns;
+    }
+
     //Create an aray of Cards
-    public Card[] createCardsCollection(int numberOfRows,int numberOfColumns,
-                                        int numberOfCards,JFrame frame){
+    public Card[] createCardsCollection(int numberOfCards){
         Card[] cards = new Card[numberOfCards];
-        JButton[] cardButtons = new JButton[numberOfCards];
+        for(int i=0 ; i<numberOfCards;i++) {
+            Color randomColor =  cardColor.generateRandomColor();
+            int colorIdntifier =randomColor.getRGB();
+            Card newCard = new Card(i,colorIdntifier);
+            newCard.setCardNumber(i);
+            newCard.setCardValue(colorIdntifier);
+            cards[i]=newCard;
+        }
+        return cards;
+    }
+
+    //Create Cards Buttons
+    public JButton[] createCardsbuttons(Card[] cards,JFrame frame){
+        JButton[] cardButtons = new JButton[cards.length];
         JPanel panel = new JPanel();
-        int padding = 5;
-        panel.setLayout(new GridLayout(numberOfRows+5,numberOfColumns+5,padding,padding));
+        JButton turnDownButton = new JButton();
+        turnDownButton.setText("Trun Down");
+        turnDownButton.setPreferredSize(new Dimension(100,100));
+        panel.setLayout(new GridLayout(this.numberOfRows+10,this.numberOfColumns+10,padding,padding));
         frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
         frame.add(panel);
+        panel.add(turnDownButton);
+        for(int i=0 ; i<cards.length;i++) {
 
-        for(int i=0 ; i<numberOfCards;i++) {
-            JButton cardButton = new JButton();
-            cardButton.setPreferredSize(new Dimension(100,50));
-            Color randomColor = generateRandomColor();
-            int colorIdntifier =randomColor.getRGB();
-            cardButton.setBackground(randomColor);
-            cardButton.setBorderPainted(false);
-            cardButton.setOpaque(true);
-            cardButton.setBorder(new EmptyBorder(padding, padding, padding, padding));
+            JButton cardButton = creatButtonCard();
             cardButtons[i]=cardButton;
-            panel.add(cardButtons[i]);
+            panel.add(cardButton);
+            int cardNumber = cards[i].getCardNumber();
+            int cardValue = cards[i].getCardValue();
+
             cardButton.addActionListener(new ActionListener() {
                 @Override
                 public void actionPerformed(ActionEvent e) {
-                    cardButton.setBackground(Color.white);
+                    cardButton.setBackground(new Color(cardValue));
                 }
+            });
+        }
+
+        turnDownButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                turnDownCards(cardButtons);
             }
-            );
-        };
+        });
         frame.revalidate();
         frame.repaint();
-        return  cards;
+        return cardButtons;
     }
-    public Color generateRandomColor(){
-        Random random = new Random();
-        Color randomcolor = new Color(random.nextInt(256), random.nextInt(256),
-                random.nextInt(256));
-                return randomcolor ;
+    public void turnDownCards(JButton[] cardButtons){
+        for(JButton cardButton : cardButtons){
+            cardButton.setBackground(Color.white);
+            cardButton.setOpaque(true);
+            cardButton.setBorderPainted(false);
+        }}
+
+    public JButton creatButtonCard(){
+        //  Color randomColor = generateRandomColor();
+        JButton cardButton = new JButton();
+        cardButton.setPreferredSize(new Dimension(100,50));
+        //cardButton.setBackground(randomColor);
+        cardButton.setBorderPainted(false);
+        cardButton.setOpaque(true);
+        cardButton.setBorder(new EmptyBorder(padding, padding, padding, padding));
+        return cardButton;
     }
 }
