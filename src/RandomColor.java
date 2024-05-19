568d6a6b8088b4feaf4c01451c56895ee898c5ac
RehamTammam1 <rtammam0@gmail.com>
2024-05-19 22:32:03 +0300

    add RandomColor Class

--- src/RandomColor.java
+++ src/RandomColor.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class RandomColor {
+import java.awt.*;
+import java.util.Random;
+
+public class RandomColor {
+
+    public Color generateRandomColor(){
+        Random random = new Random();
+        Color randomcolor = new Color(random.nextInt(256), random.nextInt(256),
+                random.nextInt(256));
+        return randomcolor ;
+    }
 }
