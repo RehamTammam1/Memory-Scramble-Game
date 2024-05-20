8b381bae3b5c5afe398ead7a995f795edeca7873
RehamTammam1 <rtammam0@gmail.com>
2024-05-20 18:41:13 +0300

    Adding generate Random Colors function

--- src/RandomColor.java
+++ src/RandomColor.java
@@ -1,5 +1,8 @@
 import java.awt.*;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Random;
+import java.util.Set;
 
 public class RandomColor {
 
@@ -8,5 +11,17 @@ public class RandomColor {
         Color randomcolor = new Color(random.nextInt(256), random.nextInt(256),
                 random.nextInt(256));
         return randomcolor ;
+    }
+
+    public Color[] generateRandomColors(int numberOfCards){
+        Color[] arrayOfColor = new Color[numberOfCards/2];
+        Set<Color> colorSet= new HashSet<>();
+
+        while (colorSet.size() < arrayOfColor.length){
+            Color randomColor = this.generateRandomColor();
+            colorSet.add(randomColor);
+        }
+        arrayOfColor = colorSet.toArray(new Color[0]);
+        return  arrayOfColor;
     }
 }
