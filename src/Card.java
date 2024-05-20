b0d123a54c5e0a6c45b41bffe289f570c9ca778b
RehamTammam1 <rtammam0@gmail.com>
2024-05-20 19:32:30 +0300

    Removing Dead Code

--- src/Card.java
+++ src/Card.java
@@ -6,45 +6,24 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Card {
-
     private int cardNumber ;
     private int cardValue ;// Card Constructor
     public Card(int cardNumber,int cardValue) {
         this.cardNumber = cardNumber;
         this.cardValue = cardValue;
-
+    }
+    // Setter and Getter  for card Number
+    public void setCardNumber(int cardNumber) {
+        this.cardNumber = cardNumber;
     }
     public int getCardNumber() {
         return cardNumber;
     }
-
-    // Setter for cardView
-    public void setCardNumber(int cardNumber) {
-        this.cardNumber = cardNumber;
+    // Setter and Getter for cardValue
+    public void setCardValue(int cardValue) {
+        this.cardValue = cardValue;
     }
     public int getCardValue() {
         return cardValue;
     }
-    // Setter for cardValue
-    public void setCardValue(int cardValue) {
-        this.cardValue = cardValue;
-    }
-    // Getter for cardView
-
-/*
-    ArrayList<JButton> cardList = new ArrayList<>();
-    public ArrayList<JButton> createCards (int cardsNumber){
-        for(int i=0;i<cardsNumber;i++){
-            JButton button = new JButton();
-            button.setText("***");
-            cardList.add(button);
-
-        }
-        return cardList;
-    }*/
-
-
-    public void showCard (){
-        System.out.println(this.cardValue);
-        System.out.println(this.cardNumber);
-    }}
+}
